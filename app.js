022d180c83c538569e861eb715ef89cf17a95498
dakhtar6 <daneyalakhtar@gmail.com>
2015-02-17 16:07:14 -0700

    Push for Eric

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+app.get('/Sevilla', function(req, res) {
+	res.render('index');
+});
+
+var server = app.listen(8162, function() {
+	console.log('Express server listening on port ' + server.address().port);
+});
---
+++ public/CanaryIslands.jade
@@ -0,0 +1,7 @@
+//- Canary Islands
+
+extends layout
+	a(href = "/Cape Verde") Next 
+	a(href = "/") Previous 
+block content
+	h1 Canary Islands
